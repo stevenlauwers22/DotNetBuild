18b329d7315f09ac39a015acdc96ace21707dee9
stevenlauwers22 <stevenlauwers22@hotmail.com>
2014-06-10 13:48:18 +0200

    Fix circular dependency problem for the generic TargetBuilder

--- DotNetBuild.Core/TargetBuilder.cs
+++ DotNetBuild.Core/TargetBuilder.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace DotNetBuild.Core
 {
@@ -11,7 +13,7 @@ namespace DotNetBuild.Core
         ITargetBuilder Do(Func<IConfigurationSettings, Boolean> executeFunc);
     }
 
-    public class TargetBuilder 
+    public class TargetBuilder
         : ITargetBuilder
     {
         private readonly ITargetRegistry _targetRegistry;
@@ -22,9 +24,33 @@ namespace DotNetBuild.Core
             if (String.IsNullOrEmpty(name))
                 throw new ArgumentNullException("name");
 
-            _target = new GenericTarget(description);
+            _target = GenerateTarget(name, description);
             _targetRegistry = targetRegistry;
             _targetRegistry.Add(name, _target);
+        }
+
+        private static GenericTarget GenerateTarget(string name, string description)
+        {
+            // Generate a class at runtime, otherwise the circular dependency resolver can't distinguish different generic targets from another
+            
+            var assemblyName = new AssemblyName(String.Format("DotNetBuild.Core.RuntimeTypes.{0}", Guid.NewGuid()));
+            var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName.Name);
+            var baseClass = typeof(GenericTarget);
+            var typeBuilder = moduleBuilder.DefineType(String.Format("{0}.{1}", assemblyName, name), TypeAttributes.Public, baseClass);
+            var constructorArguments = new[] { typeof(string) };
+            var constructorBuilder = typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, constructorArguments);
+            var constructorGenerator = constructorBuilder.GetILGenerator();
+
+            // Call the constructor of the base class with description as parameter
+            constructorGenerator.Emit(OpCodes.Ldarg_0);
+            constructorGenerator.Emit(OpCodes.Ldarg_1);
+            constructorGenerator.Emit(OpCodes.Call, baseClass.GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, constructorArguments, null));
+            constructorGenerator.Emit(OpCodes.Ret);
+
+            var type = typeBuilder.CreateType();
+            var target = (GenericTarget)Activator.CreateInstance(type, new object[] { description });
+            return target;
         }
 
         public ITarget GetTarget()
@@ -50,13 +76,13 @@ namespace DotNetBuild.Core
             return this;
         }
 
-        public class GenericTarget
+        public abstract class GenericTarget
             : ITarget
         {
             private readonly String _description;
             private readonly IList<Func<ITarget>> _dependsOn;
 
-            public GenericTarget(String description)
+            protected GenericTarget(String description)
             {
                 _description = description;
                 _dependsOn = new List<Func<ITarget>>();
